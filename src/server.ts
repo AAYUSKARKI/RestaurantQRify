38753033d3f2b63d9e8f57635589278a24f06f9e
aayus karki <karki.aayush2003@icloud.com>
2025-12-14 09:50:12 +0545

    feat: add openAPIRouter in main app

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,7 @@ import express, { type Express } from "express";
 import cors from "cors";
 import helmet from "helmet";
 import http from "http";
+import { openAPIRouter } from "./api-docs/openAPIRouter";
 
 const app: Express = express();
 const server = http.createServer(app);
@@ -14,5 +15,7 @@ app.use(cors({
 }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use(openAPIRouter);
 
 export { server };
